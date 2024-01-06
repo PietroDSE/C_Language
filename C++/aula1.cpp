0986bd9b70eb96138cb317a1cf89c69728175941
Pietro d'Avila <dsescuderop@gmail.com>
2024-01-06 16:39:15 -0300

    asdo

---
+++ C++/aula1.cpp
@@ -0,0 +1,6 @@
+#include <iostream> // biblioteca padrão C++
+
+int main() {
+    std::cout << "Hello, World!" << std::endl; //cria uma linha com o comando "Hello, world!"
+    return 0; //chama a primeira função {main()}
+}
